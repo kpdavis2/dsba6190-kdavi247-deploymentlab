2e508b445213f952844056a236eb06a5c6125455
kpdavis2 <kpd2001@charter.net>
2024-11-13 19:58:42 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -37,7 +37,7 @@ variable "environment" {
 variable "student_name" {
   description = "Application Name"
   type        = string
-  default     = "kdavi247"
+  default     = "kdavi24"
 }
 
 variable "class_name" {
